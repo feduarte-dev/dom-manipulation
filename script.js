245e83c8b4ddeb2dbd2a7147744208d159437ed5
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-11 18:28:37 -0300

    quinto exercicio completo

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ ex2[0].style.backgroundColor = "rgb(76, 164, 109)";
 const ex3 = document.querySelectorAll(".main-content .center-content");
 ex3[0].style.backgroundColor = "white";
 
+// Exercício 4
+const ex4 = document.getElementsByClassName("title");
+ex4[0].innerHTML = 'Desafio - JavaScript';
+
+// Exercício 5
+const ex5 = document.querySelector('.center-content p');
+ex5.innerHTML = 'TEXTO PADRÃO DO NOSSO SITE'
