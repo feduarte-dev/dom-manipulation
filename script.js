ed6413df11e3c81dac16b39abc071b8fe09fccc2
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-11 18:21:55 -0300

    terceiro requisito completo

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Seu código aqui
-const array = document.querySelectorAll(".center-content p");
-array[1].innerHTML = "Rico";
-console.log(array[1]);
+// Exercício 1
+const ex1 = document.querySelectorAll(".center-content p");
+ex1[1].innerHTML = "Rico";
+
+// Exercício 2
+const ex2 = document.getElementsByClassName("main-content");
+ex2[0].style.backgroundColor = "rgb(76, 164, 109)";
+
+// Exercício 3
+const ex3 = document.querySelectorAll(".main-content .center-content");
+ex3[0].style.backgroundColor = "white";
 
