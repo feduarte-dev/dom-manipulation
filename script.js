7435b823a225b291e5aeddaf71c59b2bdda42348
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-12 10:32:47 -0300

    ultimo exercicio refatorado

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ ex5.innerHTML = "TEXTO PADRÃO DO NOSSO SITE";
 // Exercício 6
 const ex6 = document.querySelectorAll(".center-content p");
 let ex7 = document.querySelector("footer p");
-let separacao = " ";
+let separacao = "";
 for (let index = 0; index < ex6.length; index += 1) {
-  let elemento = document.createTextNode(" " + ex6[index].innerHTML);
-  ex7.appendChild(elemento);
+  separacao += ex6[index].innerText + " ";
+  // Código commitado anteriormente é diferente do atual, recebi ajuda do Nicolas na monitoria summer para refatorar o conteudo do loop.
 }
+ex7.innerText = separacao;
